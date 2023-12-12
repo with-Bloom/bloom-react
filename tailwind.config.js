7f54f2d6e7e9ec9995a9f81bfd9a417316ad4c33
saseungg <yeon14766@gmail.com>
2023-12-12 23:13:30 +0900

    design: custom color 변수 지정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray100: '#F7F8FA',
+        gray200: '#B0B8C1',
+        gray300: '#3E3E3E',
+        gray400: '#30343E',
+        gray500: '#191F28',
+        indigo: '#7990F6',
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
