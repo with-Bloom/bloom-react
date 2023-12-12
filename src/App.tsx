7b3e8a1b9cd7168d03359f46352e9d648ed0fe64
saseungg <yeon14766@gmail.com>
2023-12-13 05:21:45 +0900

    feat: page에 따라 다른 컴포넌트 표시 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './pages/Home';
+import HomePage from 'pages/Home';
+import QuestionPage from 'pages/Quesiton';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
+        <Route path="/question" element={<QuestionPage />}></Route>
       </Routes>
     </Router>
   );
--- src/pages/Quesiton/index.tsx
+++ src/pages/Quesiton/index.tsx
@@ -1,20 +1,53 @@
+import { useState } from 'react';
+import { questionList } from 'src/data/questionList';
 import Layout from 'layout/index';
 import QuestionTitle from 'components/QuestionTitle';
 import Header from 'components/Header';
 import Input from 'components/Input';
+import PrimaryButton from 'components/Button/PrimaryButton';
 import NextButton from 'components/Button/NextButton';
 
+const selectComponent = ({ type, options }) => {
+  switch (type) {
+    case 'input':
+      return (
+        <>
+          <Input placeholder={options} />
+          <NextButton>다음</NextButton>
+        </>
+      );
+    case 'image-card-button':
+      return <div>이미지 카드</div>;
+    case 'image-button':
+      return <div>이미지</div>;
+    case 'primary-button':
+      return <PrimaryButton>{options}</PrimaryButton>;
+  }
+};
+
 const QuestionPage = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const LAST_PAGE = 9;
   const text = '축사를 할 사람의 \n 이름을 알려 주세요.';
   return (
     <>
       <Layout>
         <div>
-          <Header/>
-          <QuestionTitle>{text}</QuestionTitle>
-          <Input placeholder="이름을 작성해주세요."></Input>
+          <Header />
+          <span>
+            {currentPage}/{LAST_PAGE}
+          </span>
+          {questionList.map((el) => {
+            return (
+              currentPage === el.page && (
+                <>
+                  <QuestionTitle>{el.question}</QuestionTitle>
+                  {selectComponent(el)}
+                </>
+              )
+            );
+          })}
         </div>
-        <NextButton>다음</NextButton>
       </Layout>
     </>
   );
