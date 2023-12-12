3b3101ffd74a4886d364a698801aeaeb5af110a6
saseungg <yeon14766@gmail.com>
2023-12-12 13:00:37 +0900

    chore: 프로젝트 초기 세팅
    - eslint, prettier 설정
    - vite, ts, tailwind, storybook 설치

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/Home';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+};
+
+export default HomePage;
