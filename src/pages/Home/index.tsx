ba1bb97240bf36a1bd2c3ca01c9fc4d175af5fe9
saseungg <yeon14766@gmail.com>
2023-12-14 12:00:54 +0900

    design: home 페이지 스타일 및 텍스트 수정

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import NextButton from 'components/Button/NextButton';
 import HomeCharacter from 'assets/images/home-character.svg?react';
 
 const HomePage = () => {
-  const title = '맞춤형 축사 문장 추천 서비스';
+  const title = 'BLOOM';
   const subTitle =
     '소중한 사람을 축복하기 위한 결혼식 축사,\n당신의 진심을 전달할 수 있도록 블룸이 도와드릴게요.';
 
@@ -13,8 +13,10 @@ const HomePage = () => {
       <div className="flex h-full flex-col items-center justify-between pb-10">
         <div className="flex h-[calc(100vh-95px)] flex-col items-center justify-center">
           <HomeCharacter />
-          <span className="font-Pretendard mb-[15px] mt-[33px] text-2xl font-bold	text-gray500">{title}</span>
-          <span className="font-Pretendard whitespace-pre-line text-center text-[15px] leading-[150%] text-gray300">
+          <span className="text-gray700 mb-[15px] mt-[33px] font-Pretendard text-[38px]	font-bold">
+            {title}
+          </span>
+          <span className="whitespace-pre-line text-center font-Pretendard text-[15px] leading-[150%] text-gray500">
             {subTitle}
           </span>
         </div>
