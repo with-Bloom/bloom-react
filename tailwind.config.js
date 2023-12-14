f1ebdf915b1649da8706ebde4aef6ce351db4c84
saseungg <yeon14766@gmail.com>
2023-12-14 11:27:29 +0900

    design: 디자인 시스템 gray 컬러 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,12 @@ export default {
     extend: {
       colors: {
         gray100: '#F7F8FA',
-        gray200: '#B0B8C1',
-        gray300: '#404349',
-        gray400: '#30343E',
-        gray500: '#191F28',
+        gray200: '#D9DEE0',
+        gray300: '#B0B8C1',
+        gray400: '#686D75',
+        gray500: '#404349',
+        gray600: '#30343E',
+        gray700: '#191F28',
         indigo: '#7990F6',
       },
     },
