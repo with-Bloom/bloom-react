61eeaaac3054a9f64b6681e8c3e5639f9fe61b63
saseungg <yeon14766@gmail.com>
2023-12-15 11:17:07 +0900

    design: 버튼 disabled시 애니메이션 추가

--- src/components/Button/NextButton/index.tsx
+++ src/components/Button/NextButton/index.tsx
@@ -10,7 +10,7 @@ const NextButton = ({ children, onClick, disabled }: Props) => {
       onClick={onClick}
       className={`bg-${
         disabled ? 'gray400' : 'gray700'
-      } flex h-[55px] w-[327px] items-center justify-center rounded-[10px] font-Pretendard text-[17px] font-bold text-white`}
+      } flex h-[55px] w-[327px] items-center justify-center rounded-[10px] font-Pretendard text-[17px] font-bold text-white transition-opacity ease-in-out`}
       disabled={disabled}
     >
       {children}
