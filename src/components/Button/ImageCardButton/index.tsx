f5dbc85d4b29ad680833da7ca4f907bcbb4318a1
saseungg <yeon14766@gmail.com>
2023-12-16 19:51:27 +0900

    design: 호버 색상 수정
    - gray -> indigo

--- src/components/Button/ImageCardButton/index.tsx
+++ src/components/Button/ImageCardButton/index.tsx
@@ -17,7 +17,7 @@ const ImageCardButton = ({ options, onClick }: Props) => {
         return (
           <button
             key={el.target}
-            className="flex h-[187px] w-[158px] flex-col items-center justify-between rounded-[5px] bg-gray100 py-6 font-Pretendard hover:bg-gray-200"
+            className="flex h-[187px] w-[158px] flex-col items-center justify-between rounded-[5px] bg-gray100 py-6 font-Pretendard hover:bg-indigo"
             onClick={() => onClick(el.target)}
           >
             <FlowerIcon fill={el.color} />
--- src/components/Button/PrimaryButton/index.tsx
+++ src/components/Button/PrimaryButton/index.tsx
@@ -13,7 +13,7 @@ const PrimaryButton = ({ options, onClick }: Props) => {
       {options.map((el) => (
         <div key={el} className="relative">
           <button
-            className={`flex h-11 w-[327px] items-center justify-items-start rounded-[5px] bg-gray100 p-[13px] py-6 font-Pretendard font-medium text-gray700 hover:bg-gray-200 ${
+            className={`flex h-11 w-[327px] items-center justify-items-start rounded-[5px] bg-gray100 p-[13px] py-6 font-Pretendard font-medium text-gray700 hover:bg-indigo ${
               el === '3분' ? 'relative' : ''
             }`}
             onClick={() => onClick(el)}
@@ -22,9 +22,9 @@ const PrimaryButton = ({ options, onClick }: Props) => {
           </button>
           {el === '3분' && (
             <div className="relative z-10">
-              <div className="absolute right-3 top-[-20px] mt-2.5 w-max rounded bg-indigo px-3 py-2.5 text-xs text-white">
+              <div className="bg-grat1100 absolute right-3 top-[-20px] mt-2.5 w-max rounded px-3 py-2.5 text-xs text-white">
                 {tooltipText}
-                <TriangleIcon className="absolute bottom-full right-0 mr-3 h-2" />
+                <TriangleIcon className="absolute bottom-[35px] right-0 mr-3 h-2" />
               </div>
             </div>
           )}
