7b59180327ec71bd2ce37e594d5714ffb447f0a7
saseungg <yeon14766@gmail.com>
2024-02-04 02:52:49 +0900

    feat: 유저 응답 타입 지정, types 폴더 경로 설정

---
+++ src/types/index.ts
@@ -0,0 +1,11 @@
+export type UserAnswer = {
+  userName: string;
+  targetName: string;
+  targetType: string;
+  relationship: string;
+  minute: number;
+  speechType: string;
+  concept: string;
+  story: string;
+  lastComment: string;
+};
