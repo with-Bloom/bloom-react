c7f18eb2ac379f18dcbde0c8334d58d09a6fc76b
saseungg <yeon14766@gmail.com>
2024-02-13 03:07:24 +0900

    style: useInput 변수명 수정

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 
 const useInput = (initialValue: string) => {
-  const [inputValue, setInputValue] = useState(initialValue);
+  const [value, setValue] = useState(initialValue);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setInputValue(e.target.value);
+    setValue(e.target.value);
   };
 
-  return [inputValue, handleChange] as const
+  return [value, handleChange] as const
 };
 
 export default useInput;
