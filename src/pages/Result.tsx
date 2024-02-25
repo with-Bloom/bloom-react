d57ea2ac202e7b60203ae4eb1248082d2a42e141
saseungg <yeon14766@gmail.com>
2024-02-26 08:42:21 +0900

    feat: 축사 재생성 버튼 클릭시 새로운 축사가 생성되도록 구현

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,7 +1,15 @@
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
+import { API_MESSAGE } from 'constants/path';
+
+import { AnswerContext } from 'context/AnswerContext';
+
+import useFetch from 'hooks/useFetch';
 
 import Header from 'components/common/Header';
 import Layout from 'components/common/Layout';
+import Loading from 'components/common/Loading';
 import CopyToClipboardButton from 'components/result/resultSection/ClipboardButton';
 import ResultContentBox from 'components/result/resultSection/ResultContentBox';
 import ResultRetryButton from 'components/result/resultSection/ResultRetryButton';
@@ -10,25 +18,50 @@ import ResultTitle from 'components/result/resultSection/ResultTitle';
 import SpeechCautionSection from 'components/result/speechCautionSection/SpeechCautionSection';
 
 const ResultPage = () => {
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate('/question');
-  };
+  const [result, setResult] = useState('');
+
   const location = useLocation();
-  const result = location.state.data.resultData;
-  const name = location.state.name;
+
+  useEffect(() => {
+    const originResult = location.state.data.resultData;
+
+    if (originResult) {
+      setResult(originResult);
+    }
+  }, []);
+
+  const { answer, handleAnswerUpdate } = useContext(AnswerContext);
+  const { fetchData, isLoading } = useFetch(API_MESSAGE, answer);
+  const name = answer.userName;
+
+  const handleRefetch = async () => {
+    handleAnswerUpdate('isRenew', true);
+
+    const data = await fetchData();
+    const newResult = data.resultData;
+
+    setResult(newResult);
+  };
 
   return (
     <Layout style="result">
-      <ResultSection>
-        <Header noCloseBtn={true} onClick={handleClick} />
-        <ResultTitle name={name} />
-        <ResultContentBox>{result}</ResultContentBox>
-        <CopyToClipboardButton copyText={result} />
-        <ResultRetryButton />
-      </ResultSection>
+      {isLoading ? (
+        <Loading isRenew={true} />
+      ) : (
+        <>
+          <ResultSection>
+            <Header noCloseBtn={true} />
+            <ResultTitle name={name} />
 
-      <SpeechCautionSection />
+            <ResultContentBox>{result}</ResultContentBox>
+
+            <CopyToClipboardButton copyText={result} />
+            <ResultRetryButton onClick={handleRefetch} />
+          </ResultSection>
+
+          <SpeechCautionSection />
+        </>
+      )}
     </Layout>
   );
 };
