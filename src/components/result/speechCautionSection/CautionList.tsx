e48f7173ca6722439f2feebd24ac767bc1a07c39
saseungg <yeon14766@gmail.com>
2024-02-26 05:32:06 +0900

    refactor: 결과 페이지 축사 주의사항 컴포넌트 분리 작업
    - cautionList, cautionTitle, SpeechCautionSection 컴포넌트 분리하여 페이지 컴포넌트 UI 분리

---
+++ src/components/result/speechCautionSection/CautionList.tsx
@@ -0,0 +1,22 @@
+import { CAUTION_CATEGORIES } from 'constants/index';
+
+const CautionList = () => {
+  return (
+    <div className="mb-[28px]">
+      {CAUTION_CATEGORIES.map((el, index) => (
+        <div className="mb-4 w-[327px] rounded-[10px] bg-gray200 px-4 py-[29px] opacity-70" key={index}>
+          <div className="mb-[6px] font-pretendard text-base font-bold text-black">{el.title}</div>
+          <ul className="pl-4 list-disc">
+            {el.description.map((desc, descIndex) => (
+              <li className="mt-[12px] text-[14px] leading-[163%] tracking-[-0.5px]" key={descIndex}>
+                {desc}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default CautionList;
---
+++ src/components/result/speechCautionSection/CautionTitle.tsx
@@ -0,0 +1,11 @@
+const CAUTION_TITLE = '축사를 읽을 때 이런 부분을\n조심해주세요';
+
+const CautionTitle = () => {
+  return (
+    <div className="mb-[14px] whitespace-pre-line font-pretendard text-[20px] font-bold leading-[28px] tracking-[-0.5px] text-gray1100">
+      {CAUTION_TITLE}
+    </div>
+  );
+};
+
+export default CautionTitle;
