0eb44923f44d9cae2b01b63aff60bb0c0903c92c
saseungg <yeon14766@gmail.com>
2024-02-26 09:53:27 +0900

    design: 축사 다시 받아보기 텍스트 포인터 스타일 추가

--- src/components/result/resultSection/ResultRetryButton.tsx
+++ src/components/result/resultSection/ResultRetryButton.tsx
@@ -5,7 +5,7 @@ type Props = {
 const ResultRetryButton = ({ onClick }: Props) => {
   return (
     <span
-      className="mt-[12px] mb-[45px] underline font-medium tracking-[-0.4px] text-sm text-gray600 font-pretendard"
+      className="cursor-pointer mt-[12px] mb-[45px] underline font-medium tracking-[-0.4px] text-sm text-gray600 font-pretendard"
       data-ga="result_retry"
       onClick={onClick}
     >
