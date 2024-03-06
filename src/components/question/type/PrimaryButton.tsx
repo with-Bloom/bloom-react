57d7f805c9bd26e82019493ae1a31808cc4c80b3
saseungg <yeon14766@gmail.com>
2024-03-06 13:31:46 +0900

    design: input, primarybutton 높이값 수정

--- src/components/question/type/PrimaryButton.tsx
+++ src/components/question/type/PrimaryButton.tsx
@@ -14,11 +14,11 @@ const TOOLTIP_TEXT = '3~4분 길이의 축사가 가장 많아요!';
 
 const PrimaryButton = ({ field, ga, options, onClick }: Props) => {
   return (
-    <ul className="flex flex-col gap-3">
+    <ul className="flex flex-col gap-4">
       {options.map(({ id, value }) => (
         <div className="relative" key={id}>
           <button
-            className={`custom-hover flex h-11 w-full items-center justify-items-start rounded-[5px] bg-gray100 p-[13px] py-6 font-pretendard text-gray1000 ${
+            className={`custom-hover flex h-[55px] w-full items-center justify-items-start rounded-[5px] bg-gray100 p-[13px] py-6 font-pretendard text-gray1000 ${
               value === '3분' ? 'relative' : ''
             }`}
             data-ga={ga}
@@ -28,7 +28,7 @@ const PrimaryButton = ({ field, ga, options, onClick }: Props) => {
           </button>
           {value === '3분' && (
             <div className="relative z-10">
-              <div className="absolute right-3 top-[-20px] mt-2.5 w-max rounded bg-gray800 px-3 py-2.5 text-xs text-white">
+              <div className="absolute right-3 top-[-25px] mt-2.5 w-max rounded bg-gray800 px-3 py-2.5 text-xs text-white">
                 {TOOLTIP_TEXT}
                 <TriangleIcon className="absolute bottom-[35px] right-0 mr-3 h-2" />
               </div>
