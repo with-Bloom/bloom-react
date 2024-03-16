87e0a3b4b8c32c6b715d436e65a7472904c3a798
saseungg <yeon14766@gmail.com>
2024-03-16 23:09:18 +0900

    feat: 디자인 변경에 따른 홈 스타일 수정, 불필요한 컴포넌트 삭제

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,15 @@
 import NavigateButton from 'components/common/NavigateButton';
-import HeroSection from 'components/home/HeroSection';
 
-const BUTTON_TEXT = '축사 만들러 가기';
+import Logo from 'assets/images/logo.svg?react';
 
 const Home = () => {
   return (
-    <div className="px-6 pb-9">
-      <HeroSection />
-      <NavigateButton path="onboarding" ga="onboarding_cta">
-        {BUTTON_TEXT}
-      </NavigateButton>
+    <div className="w-full h-screen bg-[url('/assets/bg_home.png')] bg-cover relative px-6 py-9">
+      <div className="flex flex-col items-center justify-center gap-3 h-[calc(100%-55px)]">
+        <Logo />
+        <p className="font-medium font-pretendard text-[18px] text-gray1000">개인 맞춤형 축사 생성 서비스</p>
+      </div>
+      <NavigateButton path="onboarding">시작하기</NavigateButton>
     </div>
   );
 };
