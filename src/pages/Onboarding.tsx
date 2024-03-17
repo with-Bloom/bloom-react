994110ab491cab6e409b3d6071abb432482a594a
saseungg <yeon14766@gmail.com>
2024-03-17 20:32:00 +0900

    assets: 온보딩 이미지 png -> webp 변경

--- src/pages/Onboarding.tsx
+++ src/pages/Onboarding.tsx
@@ -3,7 +3,7 @@ import Slider from 'components/home/Slider';
 
 const slides = [
   {
-    image: '/assets/onboarding_1.png',
+    image: '/assets/onboarding_1.webp',
     description: {
       title: '막막한 축사 준비를 손쉽게',
       subTitle: '소중한 사람의 축복을 위해 진심을 전할 수 있도록\nAI가 맞춤형 축사 문장을 바로 생성해 줘요.',
@@ -11,14 +11,14 @@ const slides = [
   },
 
   {
-    image: '/assets/onboarding_2.png',
+    image: '/assets/onboarding_2.webp',
     description: {
       title: '축사 주의사항도 미리 살펴보세요',
       subTitle: '축사가 막막하게 느껴진다면, 주의사항을\n미리 살펴보고 걱정을 덜어보세요.',
     },
   },
   {
-    image: '/assets/onboarding_3.png',
+    image: '/assets/onboarding_3.webp',
     description: {
       title: '뻔하지 않은, 진심을 담은 축사',
       subTitle: '내 상황에 맞춰, 단 하나밖에 없는\n축사 문장을 만들어 드려요.',
@@ -41,10 +41,10 @@ const Onboarding = () => {
           {slides.map(({ image, description }, index) => (
             <div key={index} className="w-[500px] flex-shrink-0 flex flex-col items-center">
               <img src={image} />
-              <p className="font-bold font-pretendard text-[23px] text-gray1000">{description.title}</p>
-              <p className="trackimg-[-0.5px] font-pretendard text-[14px] text-gray800 whitespace-pre-line">
-                {description.subTitle}
-              </p>
+                <p className="font-bold font-pretendard text-[23px] text-gray1000">{description.title}</p>
+                <p className="trackimg-[-0.5px] font-pretendard text-[14px] text-gray800 whitespace-pre-line">
+                  {description.subTitle}
+                </p>
             </div>
           ))}
         </Slider>
