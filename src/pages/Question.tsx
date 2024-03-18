ced253c1595b052e932a076511edf77324a11a17
saseungg <yeon14766@gmail.com>
2024-03-19 00:39:11 +0900

    feat: step 내용이 아닌 스텝 번호로 변경, header, progressBar 기능 처리

--- src/pages/Question.tsx
+++ src/pages/Question.tsx
@@ -1,5 +1,10 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import { ROUTES_PATH } from 'constants/routes';
+
+import Header from 'components/common/Header';
+import ProgressBar from 'components/question/ProgressBar';
 import Step1 from 'components/question/step/Step1';
 import Step2 from 'components/question/step/Step2';
 import Step3 from 'components/question/step/Step3';
@@ -10,11 +15,24 @@ import Step7 from 'components/question/step/Step7';
 import Step8 from 'components/question/step/Step8';
 import Step9 from 'components/question/step/step9';
 
+type Steps = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+
 const Question = () => {
-  const [step, setStep] = useState<1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9>(1);
+  const navigate = useNavigate();
+
+  const [step, setStep] = useState<Steps>(1);
+
+  const prevStep = () => {
+    if (step === 1) {
+      navigate(ROUTES_PATH.home);
+    }
+    setStep(step - 1); //TODO: 타입 설정
+  };
+
   return (
-    <div className="h-full px-6 py-9">
-      <div className="w-full h-[84px] bg-slate-400">헤더, 프로그래스바 영역</div>
+    <div className="h-full px-6 pb-9">
+      <Header onPrev={prevStep} />
+      <ProgressBar currentStep={step} />
       {step === 1 && <Step1 onNext={() => setStep(2)} />}
       {step === 2 && <Step2 onNext={() => setStep(3)} />}
       {step === 3 && <Step3 onNext={() => setStep(4)} />}
